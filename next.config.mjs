f9b7fcff8bdd7db15afa61053e0a456562160dd0
Mek07 <mek0987279538@hotmail.com>
2024-04-13 03:47:56 +0700

    update

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     // ... other configurations (if any)
+//     esModuleInterop: {
+//       enabled: true, // Enable ES module interoperability
+//     },
+//   };
+  
+//   export default nextConfig;
---
+++ pages/page.js
@@ -0,0 +1,8 @@
+import { useEffect, useRef, useState } from "react";
+import "tailwindcss/tailwind.css";
+
+function page() {
+  return <div>My page</div>;
+}
+
+export default page;
