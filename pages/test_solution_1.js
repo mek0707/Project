052d274fb16f05b2225c5bbdef93a5f9821ca840
Mek07 <mek0987279538@hotmail.com>
2024-04-23 18:16:56 +0700

    update

--- pages/test_solution_1.js
+++ pages/test_solution_1.js
@@ -3,9 +3,8 @@ import "tailwindcss/tailwind.css";
 
 function ItemLook({index, name}) {
   return (
-    <div className="border-2 mx-4 flex h-[60px] items-center gap-4 hover:bg-[#d3cecd] hover:cursor-pointer bg-[#FAF3F0]">
-      <span className="col-span-1 bg-slate-300 h-full w-[30px] flex justify-center items-center">{index}</span>
-      <span className="col-span-4 flex justify-center items-center">{name}</span>
+    <div className="border-2 h-[40px] gap-4 hover:cursor-pointer flex justify-center items-center ">
+      <span className="pl-2">{name}</span>
     </div>
   );
 }
@@ -28,16 +27,37 @@ function test_solution() {
   const [dataTemporary, setDataTemporary] = useState([]);
   const [lastInteractionTime, setLastInteractionTime] = useState(null);
   console.log("lastInteractionTime", lastInteractionTime);
+
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       if (lastInteractionTime >= 5000) {
+        // setDataTemporary((prevData) => {
+        //   const newData = prevData.filter((item , index) => {
+        //     setDataTest((prevData) => [...prevData, item[index]]);
+        //     console.log('dataTest',dataTest)
+        //   });
+
+        //   return newData;
+        // });
         setDataTemporary((prevData) => {
-          const newData = prevData.filter((item) => {
-            setDataTest((prevData) => [...prevData, item]);
-            // return false;
-          });
+          const newData = prevData.filter((item, index) => item);
+        
+          let currentIndex = 0;
+          const intervalId = setInterval(() => {
+            if (currentIndex < newData.length) {
+              const itemToSend = newData[currentIndex];
+        
+              setDataTest((prevData) => [...prevData, itemToSend]);
+              setDataTemporary((prevData) => prevData.filter((item) => item !== itemToSend));
+              currentIndex++;
+            } else {
+              clearInterval(intervalId);
+            }
+          }, 1000);
+        
           return newData;
         });
+
       }
     }, 5000);
 
@@ -51,8 +71,6 @@ function test_solution() {
       setDataTest((prevData) => prevData.filter((i) => i !== item));
       setDataTemporary((prevData) => [...prevData, item]);
       setLastInteractionTime(Date.now());
-      // console.log("Data", dataTest);
-      // console.log("Temporary Data", dataTemporary);
     }
   }
 
@@ -66,17 +84,13 @@ function test_solution() {
     });
     setLastInteractionTime(Date.now());
     setDataTemporary((prevDataTemporary) => prevDataTemporary.filter((i) => i !== item));
-    // console.log("Data", dataTest);
-    // console.log("Temporary Data", dataTemporary);
   };
 
   return (
     <div>
-      <div className="text-right text-[20px] font-bold px-2 pt-2 mr-4 underline">Support Responsive View</div>
-      <div className={`grid grid-cols-2 lg:grid-cols-3 gap-4 text-center p-4`}>
-        <div className="col-span-2 lg:col-span-1 border bg-[#D4E2D4]">
-          <div className="mb-4 p-4 bg-[#A0C49D] font-bold">Auto Delete Todo List</div>
-          <div className={`flex flex-col gap-4 h-[30vh] lg:h-[60vh] overflow-auto mb-4`}>
+      <div className={`grid grid-cols-3 gap-2 text-center p-4`}>
+        <div className="col-span-1">
+          <div className={`flex flex-col gap-2 h-[40px] mb-4`}>
             {dataTest.map((item, index) => (
               <div key={index} onClick={() => handleItemClick(item, false)}>
                 <ItemLook index={index + 1} name={item.name} />
@@ -84,9 +98,9 @@ function test_solution() {
             ))}
           </div>
         </div>
-        <div className="col-span-1 border bg-[#D4E2D4]">
-          <div className="mb-4 p-4 font-bold bg-[#A0C49D]">Fruit</div>
-          <div className={`flex flex-col gap-4 h-[30vh] lg:h-[60vh] overflow-auto mb-4`}>
+        <div className="col-span-1 h-[90vh] ml-4">
+          <div className="h-[40px] bg-gray-100 flex items-center justify-center">Fruit</div>
+          <div className={`flex flex-col gap-2 h-full mb-4 border p-2`}>
             {dataTemporary.map((item, index) => {
               if (item.type === "Fruit") {
                 return (
@@ -98,9 +112,9 @@ function test_solution() {
             })}
           </div>
         </div>
-        <div className="col-span-1 border bg-[#D4E2D4]">
-          <div className="mb-4 p-4 font-bold bg-[#A0C49D]">Vegetable</div>
-          <div className={`flex flex-col gap-4 h-[30vh] lg:h-[60vh] overflow-auto mb-4`}>
+        <div className="col-span-1 h-[90vh]">
+          <div className="h-[40px] bg-gray-100 flex items-center justify-center">Vegetable</div>
+          <div className={`flex flex-col gap-2 h-full mb-4 border p-2`}>
             {dataTemporary.map((item, index) => {
               if (item.type === "Vegetable") {
                 return (
